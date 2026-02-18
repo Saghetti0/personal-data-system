ee00cc78f5a46e4170fb0eee3bf6a1353e823614
Walter Min <weeb.saghetti@gmail.com>
2026-02-18 04:11:42 -0500

    switch from native android to webstack... (capacitor+svelte)

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.waltermin.pds',
+  appName: 'PDS',
+  webDir: 'dist'
+};
+
+export default config;
